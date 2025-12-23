29601216b3107b3e09670c9f61a768f8b02b892d
hridyeshh <hridyesh2309@gmail.com>
2025-12-23 21:12:29 +0530

    Initial Commit

---
+++ PaperBoxd/PaperBoxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperBoxdApp.swift
+//  PaperBoxd
+//
+//  Created by Hridyesh on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PaperBoxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
